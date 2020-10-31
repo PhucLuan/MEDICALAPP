a0f7d85fc4190d2ed6abfd9da97174448717cd31
PhucLuan <54372656+PhucLuan@users.noreply.github.com>
2020-10-31 19:47:50 +0700

    update Homepage

---
+++ Homepage/Homepage.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", () => {
+    new Glide(".banner", {
+      type: "carousel",
+      startAt: 0,
+      animationTimingFunc: "ease-in-out",
+      perView: 1
+    }).mount();
+
+    new Glide(".typeService", {
+        type: "carousel",
+        startAt: 0,
+        animationTimingFunc: "ease-in-out",
+        perView: 4
+      }).mount();
+
+    new Glide(".bannerService", {
+        type: "carousel",
+        startAt: 0,
+        animationTimingFunc: "ease-in-out",
+        perView: 1
+    }).mount();
+
+    new Glide(".bannerhotDoctor", {
+        type: "carousel",
+        startAt: 0,
+        animationTimingFunc: "ease-in-out",
+        perView: 2
+    }).mount();
+
+    new Glide(".bannerhotHospital", {
+        type: "carousel",
+        startAt: 0,
+        animationTimingFunc: "ease-in-out",
+        perView: 2
+    }).mount();
+
+    new Glide(".bannerBlog", {
+        type: "carousel",
+        startAt: 0,
+        animationTimingFunc: "ease-in-out",
+        perView: 2
+    }).mount();
+  });
