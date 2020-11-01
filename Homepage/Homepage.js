9d0f68dacb5dc160267d56cbdbc69d42ac135c16
PhucLuan <54372656+PhucLuan@users.noreply.github.com>
2020-11-01 17:30:35 +0700

    update listHostpital and chat

--- Homepage/Homepage.js
+++ Homepage/Homepage.js
@@ -17,27 +17,27 @@ document.addEventListener("DOMContentLoaded", () => {
         type: "carousel",
         startAt: 0,
         animationTimingFunc: "ease-in-out",
-        perView: 1
+        perView: 1.2
     }).mount();
 
     new Glide(".bannerhotDoctor", {
         type: "carousel",
         startAt: 0,
         animationTimingFunc: "ease-in-out",
-        perView: 2
+        perView: 1.5
     }).mount();
 
     new Glide(".bannerhotHospital", {
         type: "carousel",
         startAt: 0,
         animationTimingFunc: "ease-in-out",
-        perView: 2
+        perView: 1.5
     }).mount();
 
     new Glide(".bannerBlog", {
         type: "carousel",
         startAt: 0,
         animationTimingFunc: "ease-in-out",
-        perView: 2
+        perView: 1.7
     }).mount();
   });
